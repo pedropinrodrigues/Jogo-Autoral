0b2409cc7cc93695feef678081a9c41a5ee2a36f
pedropinrodrigues <pedro.rodrigues@sou.inteli.edu.br>
2024-03-04 22:50:19 -0300

    segunda commit + cometarios

--- Cenas/cena01.js
+++ Cenas/cena01.js
@@ -1,3 +1,5 @@
+//Iniciando a Cena
+
 class cena01 extends Phaser.Scene {
     constructor() {
         super('cena01');
@@ -5,6 +7,9 @@ class cena01 extends Phaser.Scene {
 
 
     preload() {
+
+        //Precarregando as imagens
+
         this.load.image('fundo02', 'assets/fundo02.png');
         this.load.spritesheet('player', 'assets/player.png', { frameWidth: 32, frameHeight: 48 });
         this.load.spritesheet('blast', 'assets/haduken.png', { frameWidth: 30, frameHeight: 25 });
@@ -15,8 +20,12 @@ class cena01 extends Phaser.Scene {
 
     create(){
 
+        //Defininfo os limites do jogo para estruturar a colisão com os 'World Bounds' depois.
+
         this.physics.world.setBounds(0, 0, 1200, 605);       
         
+        //Adicionando as imagens, fundo, personagem e seu poder.
+
         this.add.image(525, 300, 'fundo02');
 
         this.player = this.physics.add.sprite(500, 605, 'player').setScale(2);
@@ -24,12 +33,18 @@ class cena01 extends Phaser.Scene {
         this.blast = this.physics.add.sprite(this.player.x, this.player.y - 40, 'blast').setVisible(false).setScale(2);
 
         this.player.setCollideWorldBounds(true);
-        this.goku.setCollideWorldBounds(true);
+        
+        //Retira o efeito da gravidade do poder, para que ele nao caia ao ser usado
+
         this.blast.body.setAllowGravity(false);
+
+        //Atribuindo as teclas a serem usadas
 
         this.cursors = this.input.keyboard.createCursorKeys();
         this.teclaC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
         
+        //Definnindo as animações do poder e do jogador
+
         this.anims.create({
             key: 'right',
             frames: this.anims.generateFrameNumbers('player', { start: 8, end: 11 }),
@@ -75,6 +90,8 @@ class cena01 extends Phaser.Scene {
       
         update(){
 
+            //Movimentação do personagem + seus sprites
+
         if (this.cursors.left.isDown)
         {
             this.player.setVelocityX(-160);
@@ -93,6 +110,8 @@ class cena01 extends Phaser.Scene {
     
             this.player.anims.play('frente');
         }
+
+        //Definindo mecanica do poder
 
         if (this.teclaC.isDown && this.cursors.right.isDown){
             this.blast.setPosition(this.player.x, this.player.y).setVisible(true).anims.play('rightblast', true);
--- Cenas/welcome.js
+++ Cenas/welcome.js
@@ -1,13 +1,22 @@
+//Iniciando a cena 
+
 class Welcome extends Phaser.Scene {
     constructor() {
         super('welcome');
     }
 
+
     preload() {
+        
+        //Carregando a imagem
+
         this.load.image('fundo01', 'assets/fundoentrada.png');
     }
 
     create() {
+
+        //Adicionando a imagem e uma área de clique, que dará inicio a segunda cena
+
         this.add.image(525, 312, 'fundo01').setScale(1.1);
 
         let inputZone = this.add.zone(400, 300, 400, 100)
